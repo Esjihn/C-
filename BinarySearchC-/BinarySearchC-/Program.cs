963def1d0f49a91dfcd2fbf34d2034c84140cbbe
Matthew Miller <35173022+Esjihn@users.noreply.github.com>
2018-09-11 08:35:04 -0400

    latestcommit

---
+++ BinarySearchC-/BinarySearchC-/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace BinarySearchC_
+{
+    class Program
+    {
+        // binary search requires data thats already sorted
+        // iF element is equal to the value, we're done
+        // if element is greater than the value, search the left
+        // half of the array, if less than, search the right of 
+        // the array.
+        // Similar to merge sort.
+        // time complexity O(logn)
+        static void Main(string[] args)
+        {
+            int[] intArray = {-22, -15, 1, 7, 20, 35, 55};
+
+            Console.WriteLine(iterativeBinarySearch(intArray, -15));
+            Console.WriteLine(iterativeBinarySearch(intArray, 35));
+            Console.WriteLine(iterativeBinarySearch(intArray, 8888));
+            Console.WriteLine(iterativeBinarySearch(intArray, 1));
+        }
+
+        static int iterativeBinarySearch(int[] input, int value)
+        {
+            int start = 0;
+            int end = input.Length;
+
+            while (start < end)
+            {
+                int midpoint = (start + end) / 2;
+                Console.WriteLine("midpoint = " + midpoint);
+                if (input[midpoint] == value)
+                {
+                    return midpoint;
+                }
+                // search right part of array
+                else if (input[midpoint] < value)
+                {
+                    // changing index not value
+                    start = midpoint + 1;
+                }
+                else
+                {
+                    end = midpoint; // search left part of array
+                }
+            }
+
+            return -1;
+        }
+
+    }
+}
---
+++ LinearSearchAlgorithm/LinearSearchAlgorithm/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace LinearSearchAlgorithm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] intArray = {20, 35, -15, 7, 55, 1, -22};
+            Console.WriteLine(linearSearch(intArray, -15));
+            Console.WriteLine(linearSearch(intArray, 1));
+            Console.WriteLine(linearSearch(intArray, 8888));
+            Console.WriteLine(linearSearch(intArray, -22));
+            //O(n) linear
+        }
+
+        static int linearSearch(int[] input, int value)
+        {
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (input[i] == value)
+                {
+                    return i;
+                }
+            }
+
+            return -1;
+        }
+
+    }
+}
