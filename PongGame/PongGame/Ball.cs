f3da4c70032c2e4ba38dbbb3bcff3c9df7c38b73
Matthew Miller <35173022+Esjihn@users.noreply.github.com>
2018-09-11 17:17:21 -0400

    PongGame
    Pong Game using WPF

---
+++ PongGame/PongGame/Ball.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PongGame
+{
+    class Ball
+    {
+        double xPosition;
+        double yPosition;
+        bool isDirectionRight;
+
+        public double XPosition
+        {
+            get => xPosition;
+            set => xPosition = value;
+        }
+
+        public double YPosition
+        {
+            get => yPosition;
+            set => yPosition = value;
+        }
+
+        public bool IsDirectionRight
+        {
+            get => isDirectionRight;
+            set => isDirectionRight = value;
+        }
+    }
+}
---
+++ PongGame/PongGame/ViewModel.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using PongGame.Annotations;
+
+namespace PongGame
+{
+    class ViewModel : INotifyPropertyChanged
+    {
+        int _leftPadPosition = 180;
+        int _rightPadPosition = 180;
+        int _leftResult = 0;
+        int _rightResult = 0;
+        Ball ball = new Ball { XPosition = 380, YPosition = 210, IsDirectionRight = true};
+
+        public int LeftPadPosition
+        {
+            get { return _leftPadPosition; }
+            set
+            {
+                _leftPadPosition = value;
+                OnPropertyChanged("LeftPadPosition");
+            }
+        }
+        public int RightPadPosition
+        {
+            get { return _rightPadPosition; }
+            set
+            {
+                _rightPadPosition = value;
+                OnPropertyChanged("RightPadPosition");
+            }
+        }
+
+        public int LeftResult
+        {
+            get { return _leftResult; }
+            set
+            {
+                _leftResult = value;
+                OnPropertyChanged("LeftResult");
+            }
+        }
+
+        public int RightResult
+        {
+            get { return _rightResult; }
+            set
+            {
+                _rightResult = value;
+                OnPropertyChanged("RightResult");
+            }
+        }
+
+        public double BallXPosition
+        {
+            get { return ball.XPosition; }
+            set
+            {
+                ball.XPosition = value;
+                OnPropertyChanged("BallXPosition");
+            }
+        }
+
+        public double BallYPosition
+        {
+            get { return ball.YPosition; }
+            set
+            {
+                ball.YPosition = value;
+                OnPropertyChanged("BallYPosition");
+            }
+        }
+
+        public bool IsBallDirectionRight
+        {
+            get { return ball.IsDirectionRight; }
+            set
+            {
+                ball.IsDirectionRight = value;
+                OnPropertyChanged("IsBallDirectionRight");
+            }
+        }
+
+        public void changeBallDirection()
+        {
+            IsBallDirectionRight = !IsBallDirectionRight;
+        }
+      
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
