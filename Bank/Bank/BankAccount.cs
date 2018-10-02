efe077c2fa072e81766d461e0c95493497ac6d3d
Matthew Miller <35173022+Esjihn@users.noreply.github.com>
2018-10-02 09:45:53 -0400

    Testing Refactoring and Unit Testing techniques

---
+++ Bank/Bank/BankAccount.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bank
+{
+    public class BankAccount
+    {
+        string m_customerName;
+        double m_balance;
+        bool m_frozen = false;
+
+        BankAccount() { }
+
+        public BankAccount(string customerName, double balance)
+        {
+            m_customerName = customerName;
+            m_balance = balance;
+        }
+
+        public string CustomerName
+        {
+            get { return m_customerName; }
+        }
+
+        public double Balance
+        {
+            get { return m_balance; }
+        }
+
+        public void Debit(double amount)
+        {
+            if (m_frozen)
+            {
+                throw new Exception("Account frozen");
+            }
+
+            if (amount > m_balance)
+            {
+                throw new ArgumentOutOfRangeException("amount");
+            }
+
+            if (amount < 0)
+            {
+                throw new ArgumentOutOfRangeException("amount");
+            }
+
+            m_balance -= amount; // intentionally incorrect code
+        }
+
+        public void Credit(double amount)
+        {
+            if (m_frozen)
+            {
+                throw new Exception("Account frozen");
+            }
+
+            if (amount < 0)
+            {
+                throw new ArgumentOutOfRangeException("amount");
+            }
+
+            m_balance += amount;
+        }
+
+        void FreezeAccount()
+        {
+            m_frozen = true;
+        }
+
+        void UnfreezeAccount()
+        {
+            m_frozen = false;
+        }
+
+        public static void Main()
+        {
+            BankAccount ba = new BankAccount("Mr. Bryan Walton", 11.99);
+
+            ba.Credit(5.77);
+            ba.Debit(11.22);
+            Console.WriteLine("Current balance is ${0}", ba.Balance);
+        }
+
+    }
+}
---
+++ Bank/BankTests/UnitTest1.cs
@@ -0,0 +1,44 @@
+using System;
+using Bank;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BankTests
+{
+    // TestClass attribute is required in the Microsoft unit testing 
+    // framework for managed code for any class that contains unit test
+    // methods that you want to run in Test Explorer
+    [TestClass]
+    public class UnitTest1
+    {
+        // and each test method that you want Test Explorer to run must have the 
+        [TestMethod]
+        public void TestMethod1()
+        {
+            
+        }
+
+        // Must meet the following requirements
+        // It's decorated with the [TestMethod] attribute.
+        // It returns void.
+        // It cannot have parameters.
+        [TestMethod]
+        public void Debit_WithValidAmount_UpdatesBalance()
+        {
+            // Arrange
+            double beginningBalance = 11.99;
+            double debitAmount = 4.55;
+            double expected = 7.44;
+            BankAccount account = new BankAccount("Mr. Bryan Walton", beginningBalance);
+
+            // Act
+            account.Debit(debitAmount);
+
+            // Assert
+            double actual = account.Balance;
+            Assert.AreEqual(expected, actual, 0.001, "Account not debited correctly");
+
+
+
+        }
+    }
+}
